17dd654d1d1ff276b48d149e3b67d505fb47a876
anast <anastasiakomar06072002@gmail.com>
2021-11-25 22:39:29 +0200

    Controller +

---
+++ src/main/java/com/example/calculator/Solution.java
@@ -0,0 +1,63 @@
+package com.example.calculator;
+
+
+class Solution {
+
+    public static String Solve(String input) {
+        String[] math = splitString(input);
+        int action = getAction(math);
+        int equal= getEqual(math);
+        double number1 = getNumber(math, 0, action);
+        double number2 = getNumber(math, action + 1, equal);
+        return ""+Result(math[action],number1,number2);
+    }
+
+    public static String[] splitString(String problem) {
+        String[] single = new String[problem.length()];
+        single = problem.split("");
+        return single;
+    }
+
+    public static int getAction(String[] single) {
+        int a = 0;
+        for (int i = 0; i < single.length; i++) {
+            if ((single[i].equalsIgnoreCase("+")) || (single[i].equalsIgnoreCase("-")) || (single[i].equalsIgnoreCase("*")) || (single[i].equalsIgnoreCase("/"))) {
+                a = i;
+            }
+        }
+        return a;
+    }
+
+    public static int getEqual(String[] single) {
+        int a = single.length;
+        for (int i = 0; i < single.length; i++) {
+            if (single[i].equalsIgnoreCase("=")) {
+                a=i;
+            }
+        }
+        return a;
+    }
+
+
+    public static double getNumber(String[] single, int startPoint, int endPoint) {
+        String number = "";
+        for (int i = startPoint; i < endPoint; i++) {
+            number = number + single[i];
+        }
+        return Double.parseDouble(number);
+    }
+
+    public static double Result(String operator, double number1, double number2) {
+        double result=0;
+        if (operator.equalsIgnoreCase("+")) {
+            result=number1 + number2;
+        } else if (operator.equalsIgnoreCase("-")) {
+            result= number1 - number2;
+        } else if (operator.equalsIgnoreCase("*")) {
+            result=number1 * number2;
+        } else if (operator.equalsIgnoreCase("/")) {
+            result= number1 / number2;
+        }
+        return result;
+    }
+}
